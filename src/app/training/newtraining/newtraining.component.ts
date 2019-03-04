eaafab8331c8723f330ed0ece5f9d46de8d009b5
Andrey Klochaniy <andrey.klochaniy.ctr@ukr.fareportal.com>
2019-03-04 12:00:19 +0200

    Fix fucking collection name

--- src/app/training/newtraining/newtraining.component.ts
+++ src/app/training/newtraining/newtraining.component.ts
@@ -3,48 +3,42 @@ import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { Subject, from } from 'rxjs';
 import { NgForm } from '@angular/forms';
-import {AngularFirestore} from '@angular/fire/firestore';
-import{Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-newtraining',
   templateUrl: './newtraining.component.html',
   styleUrls: ['./newtraining.component.css']
 })
 export class NewtrainingComponent implements OnInit {
-  allExercises:any[];
+  allExercises: any;
   availableExercicesChanged = new Subject<any[]>();
 
-  constructor(private trainingService:TrainingService,private angularFireStore:AngularFirestore ) { 
-   
+  constructor(private trainingService: TrainingService, private angularFireStore: AngularFirestore) {
+
   }
 
   ngOnInit() {
-    
-    this.angularFireStore.collection('availablesExercices')
-    .snapshotChanges()
-    .pipe(map( docArray => {
-      return docArray.map( doc => {
-        console.log(doc);
-        return(
-           {
-           id: doc.payload.doc.id,
-           name: doc.payload.doc.data()['name'],
-           duration: doc.payload.doc.data()['duration'],
-           calories: doc.payload.doc.data()['calories'],
-         }
-       
-      );
-      });
-    }))
-    .subscribe(exercices => {
-      this.allExercises = exercices;
-      this.availableExercicesChanged.next([...this.allExercises]);
-    });
+    this.allExercises = this.angularFireStore
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(map(docArray => {
+        return docArray.map(doc => {
+
+          return ({
+            id: doc.payload.doc.id,
+            name: doc.payload.doc.data()['name'],
+            duration: doc.payload.doc.data()['duration'],
+            calories: doc.payload.doc.data()['calories'],
+          });
+
+        });
+      }));
   }
 
-  onStartTraining(form:NgForm)
-  {
+  onStartTraining(form: NgForm) {
     console.log(form);
     this.trainingService.startExercise(form.value.exercise);
   }
